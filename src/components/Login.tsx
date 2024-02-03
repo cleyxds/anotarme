6a123f51b0dc65dc58fc7f985db1009e2a9a69c2
Cleyson Barbosa <cleysonn@hotmail.com.br>
2024-02-03 16:53:12 -0300

    FIX: fix Login flow

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,26 +4,67 @@ import styled from "styled-components"
 
 import { Link, useNavigate, useSearchParams } from "react-router-dom"
 
+import { useSetRecoilState } from "recoil"
+import { AuthAtom } from "../atoms/Auth"
+import { UserAtom } from "../atoms/User"
+
 import { Screen } from "../ui/Screen"
 import { Button } from "../ui/atoms/Button"
 import { Footer } from "./Footer"
-import { verifyToken } from "../services/user"
+
+import { getMe, verifyToken } from "../services/user"
+
+import { Auth } from "../types/auth"
+import { User } from "../types/user"
 
 export function Login() {
-  const REDIRECT_URL = "http://localhost:5173/auth/login"
-  const AUTH_URL = `https://barbosarecipes-auth-2zf2mk77yq-uc.a.run.app/auth/login?redirectUrl=${REDIRECT_URL}`
+  const REDIRECT_URL = "http://192.168.0.104:5173/auth/login"
+  const DEV_AUTH_URL = "http://192.168.0.104:4444/auth/login"
+  const PROD_AUTH_URL =
+    "https://barbosarecipes-auth-2zf2mk77yq-uc.a.run.app/auth/login"
+  const AUTH_URL = `${DEV_AUTH_URL}?redirectUrl=${REDIRECT_URL}`
 
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
+
+  const setAuthAtom = useSetRecoilState(AuthAtom)
+  const setUserAtom = useSetRecoilState(UserAtom)
 
   const PARAMS_TOKEN = searchParams.get("token")
 
   useEffect(() => {
     if (!PARAMS_TOKEN) return
 
+    const handleRedirect = (data: any) => {
+      navigate("/chats")
+
+      return data
+    }
+
+    const handleError = (error: any) => {
+      console.error(error)
+
+      navigate("/")
+    }
+
+    const authUser = (authData: Auth) => {
+      setAuthAtom({ ...authData, isAuthenticated: true })
+
+      return authData.accessToken
+    }
+
+    const setUser = (user: User) => {
+      setUserAtom(user)
+
+      return user
+    }
+
     verifyToken(PARAMS_TOKEN)
-      .then(() => navigate("/chats"))
-      .catch(() => navigate("/"))
+      .then(authUser)
+      .then(getMe)
+      .then(setUser)
+      .then(handleRedirect)
+      .catch(handleError)
   }, [PARAMS_TOKEN, navigate])
 
   return (
