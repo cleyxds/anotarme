5293b55e37a000836efbb1d4cb0690605aa8acc1
Cleyson B <cleysonn@hotmail.com.br>
2024-05-22 12:24:13 -0300

    FEAT: Add firebase analytics

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
+import { getAnalytics } from "firebase/analytics"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 import FIREBASE_CONFIG from "../config/firebase.config.json"
@@ -8,3 +9,4 @@ export const app = initializeApp(FIREBASE_CONFIG)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
 export const GoogleProvider = new GoogleAuthProvider()
+export const analytics = getAnalytics(app)
